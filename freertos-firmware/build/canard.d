b2a7c2b120186d8f6f2d11860e90363b7dc23bd9
traffael <tschui@kth.se>
2019-04-26 23:42:08 +0200

    gitignore working

--- freertos-firmware/build/canard.d
+++
@@ -1,7 +0,0 @@
-build/canard.o: Middlewares/Third_Party/libcanard/canard.c \
- Middlewares/Third_Party/libcanard/canard_internals.h \
- Middlewares/Third_Party/libcanard/canard.h
-
-Middlewares/Third_Party/libcanard/canard_internals.h:
-
-Middlewares/Third_Party/libcanard/canard.h:
